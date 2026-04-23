010673bbebab88c0643cbb06e3f6ac77791133ff
Vinicius Dutra <winicius_dutra@hotmail.com>
2026-04-23 16:46:58 -0300

    configurado swager

--- SIGEUS/Program.cs
+++ SIGEUS/Program.cs
@@ -18,6 +18,10 @@ public class Program
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/openapi/v1.json", "API v1");
+            });
         }
 
         app.UseHttpsRedirection();
